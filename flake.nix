3476e964d1096549bdfc0da8a8da86a6fe859266
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-08-31 12:23:14 -0700

    override pnpm to be 11.24.0

--- flake.nix
+++ flake.nix
@@ -6,14 +6,26 @@
   };
 
   outputs = inputs: {
-    devShells = builtins.mapAttrs (system: pkgs: {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          nodejs_24
-          pnpm_11
-        ];
-      };
-    }) inputs.nixpkgs.legacyPackages;
+    devShells = builtins.mapAttrs (
+      system: pkgs:
+      let
+        pnpm_11_24 = pkgs.pnpm_11.overrideAttrs (old: rec {
+          version = "11.24.0";
+          src = pkgs.fetchurl {
+            url = "https://registry.npmjs.org/pnpm/-/pnpm-${version}.tgz";
+            hash = "sha256-0eqyQzFyZhzDahjshfzpP3cdsZYnFzKcwB7JwoJMok8=";
+          };
+        });
+      in
+      {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs_24
+            pnpm_11_24
+          ];
+        };
+      }
+    ) inputs.nixpkgs.legacyPackages;
 
     formatter = builtins.mapAttrs (system: pkgs: pkgs.nixfmt-tree) inputs.nixpkgs.legacyPackages;
   };

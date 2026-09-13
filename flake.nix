41c4ef4d42611e2e7218ad70c5f96484c393b102
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-09-13 11:32:39 -0700

    upgrade to pnpm 12

--- flake.nix
+++ flake.nix
@@ -8,20 +8,11 @@
   outputs = inputs: {
     devShells = builtins.mapAttrs (
       system: pkgs:
-      let
-        pnpm_11_24 = pkgs.pnpm_11.overrideAttrs (old: rec {
-          version = "11.24.0";
-          src = pkgs.fetchurl {
-            url = "https://registry.npmjs.org/pnpm/-/pnpm-${version}.tgz";
-            hash = "sha256-0eqyQzFyZhzDahjshfzpP3cdsZYnFzKcwB7JwoJMok8=";
-          };
-        });
-      in
       {
         default = pkgs.mkShell {
           packages = [
             pkgs.nodejs_24
-            pnpm_11_24
+            pkgs.pnpm_12
           ];
         };
       }

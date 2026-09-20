4c25ec630cca08be01ff78414e94aba5aaa970db
Adriana <16786568+Adrianamm@users.noreply.github.com>
2026-09-20 13:51:49 -0400

    3rd time?

--- examples/stage0/templates/src/loops.java
+++ examples/stage0/templates/src/loops.java
@@ -16,7 +16,7 @@ class Drivetrain {
 Drivetrain drivetrain = new Drivetrain();
 
 void main() {
-  
+
     // First, set calibrationTime to 0
     // Below, create a while loop that checks if calibrationTime is less than 5
     // if that is true, print "Calibrating", then increase calibrationTime by one
@@ -26,24 +26,26 @@ void main() {
 
 
 
-    
+
+
 
     // Create a for loop that has a new integer variable named `timer`
     // that is set to 15. Check for when `timer` is greater than or equal to 0,
-    // then decrease `timer` by one. 
+    // then decrease `timer` by one.
     // Inside the for loop, include a print statement that prints "time left "
     // and the variable `timer`
-  
 
 
 
 
-    // First, create a for loop that has a new integer variable named 
-    // `timer` that is set to 15. The for loop checks if `timer` is 
+
+
+    // First, create a for loop that has a new integer variable named
+    // `timer` that is set to 15. The for loop checks if `timer` is
     // greater than or equal to 0, then it decreases `timer` by one
     // Inside the for loop, have an if statement that checks if `timer`
     // is less than or equal to 0. If so, set the drivetrain speed to 0
     // If the `timer` is less than or equal to 15, set the speed to one
 
-   
+
 }
